4b98e91845a7aeba731be8e839bf775642026101
Damian Flynn <info@damianflynn.com>
2019-02-04 23:55:57 +0000

    Fixing the bash script

--- Reference/Striesand/refStriesand.sh
+++ Reference/Striesand/refStriesand.sh
@@ -1,74 +1,49 @@
 #!/bin/bash
 
-updateOs()
-{
-  sudo apt-get update
-  sudo apt-get upgrade
-}
+sudo apt-get update --assume-yes
+sudo apt-get upgrade --assume-yes
+sudo apt-get install --assume-yes git python-pip python-cffi libffi-dev libssl-dev libcurl4-openssl-dev
+pip install --upgrade pip
 
-insallPythonChain()
-{
-  sudo apt-get install git python-pip python-cffi libffi-dev libssl-dev libcurl4-openssl-dev
-  pip install --upgrade pip
-}
+git clone https://github.com/StreisandEffect/streisand.git && cd streisand
+./util/venv-dependencies.sh ./venv
+source ./venv/bin/activate
 
-bootstrapStreisand()
-{
-  git clone https://github.com/StreisandEffect/streisand.git && cd streisand
-  ./util/venv-dependencies.sh ./venv
-  source ./venv/bin/activate
-}
+# Create configuration for striesand installation
+echo '                                                               ' >> global_vars/noninteractive/my-vpn-server.yml
+echo '# Custom Server Configuration                                  ' >> global_vars/noninteractive/my-vpn-server.yml
+echo '#                                                              ' >> global_vars/noninteractive/my-vpn-server.yml
+echo '                                                               ' >> global_vars/noninteractive/my-vpn-server.yml
+echo 'streisand_noninteractive: true                                 ' >> global_vars/noninteractive/my-vpn-server.yml
+echo 'confirmation: true                                             ' >> global_vars/noninteractive/my-vpn-server.yml
+echo '                                                               ' >> global_vars/noninteractive/my-vpn-server.yml
+echo '# Change this to the location of a key on the local system.    ' >> global_vars/noninteractive/my-vpn-server.yml
+echo 'streisand_ssh_private_key: "~/.ssh/id_rsa"                     ' >> global_vars/noninteractive/my-vpn-server.yml
+echo '                                                               ' >> global_vars/noninteractive/my-vpn-server.yml
+echo 'vpn_clients: 5                                                 ' >> global_vars/noninteractive/my-vpn-server.yml
+echo '                                                               ' >> global_vars/noninteractive/my-vpn-server.yml
+echo 'streisand_openconnect_enabled: no                              ' >> global_vars/noninteractive/my-vpn-server.yml
+echo 'streisand_openvpn_enabled: no                                  ' >> global_vars/noninteractive/my-vpn-server.yml
+echo 'streisand_shadowsocks_enabled: yes                             ' >> global_vars/noninteractive/my-vpn-server.yml
+echo 'streisand_ssh_forward_enabled: yes                             ' >> global_vars/noninteractive/my-vpn-server.yml
+echo '# By default sshuttle is disabled, it creates a `sshuttle` user' >> global_vars/noninteractive/my-vpn-server.yml
+echo '# that has full shell privileges on the Streisand host         ' >> global_vars/noninteractive/my-vpn-server.yml
+echo 'streisand_sshuttle_enabled: no                                 ' >> global_vars/noninteractive/my-vpn-server.yml
+echo 'streisand_stunnel_enabled: no                                  ' >> global_vars/noninteractive/my-vpn-server.yml
+echo 'streisand_tinyproxy_enabled: yes                               ' >> global_vars/noninteractive/my-vpn-server.yml
+echo 'streisand_tor_enabled: no                                      ' >> global_vars/noninteractive/my-vpn-server.yml
+echo 'streisand_wireguard_enabled: yes                               ' >> global_vars/noninteractive/my-vpn-server.yml
+echo '                                                               ' >> global_vars/noninteractive/my-vpn-server.yml
+echo '# Definitions needed for Lets Encrypt HTTPS certificate setup. ' >> global_vars/noninteractive/my-vpn-server.yml
+echo '#                                                              ' >> global_vars/noninteractive/my-vpn-server.yml
+echo '# If these are both left as empty strings, Lets Encrypt will   ' >> global_vars/noninteractive/my-vpn-server.yml
+echo '# not be set up and a self-signed certificate will be used     ' >> global_vars/noninteractive/my-vpn-server.yml
+echo '#                                                              ' >> global_vars/noninteractive/my-vpn-server.yml
+echo '# The domain to use for Lets Encrypt certificate.              ' >> global_vars/noninteractive/my-vpn-server.yml
+echo 'streisand_domain_var: ""                                       ' >> global_vars/noninteractive/my-vpn-server.yml
+echo '# The admin email address for Lets Encrypt certificate reg     ' >> global_vars/noninteractive/my-vpn-server.yml
+echo 'streisand_admin_email_var: ""                                  ' >> global_vars/noninteractive/my-vpn-server.yml
+echo '                                                               ' >> global_vars/noninteractive/my-vpn-server.yml
 
-configureStreisand()
-{
-   # Create configuration for striesand installation
-   cat <<'EOF_VPNSERVER' >> global_vars/noninteractive/my-vpn-server.yml
-# Custom Server Configuration
-#
-
-streisand_noninteractive: true
-confirmation: true
-
-# Change this to the location of a key on the local system.
-streisand_ssh_private_key: "~/.ssh/id_rsa"
-
-vpn_clients: 5
-
-streisand_openconnect_enabled: no
-streisand_openvpn_enabled: no
-streisand_shadowsocks_enabled: yes
-streisand_ssh_forward_enabled: yes
-# By default sshuttle is disabled because it creates a `sshuttle` user that has
-# full shell privileges on the Streisand host
-streisand_sshuttle_enabled: no
-streisand_stunnel_enabled: no
-streisand_tinyproxy_enabled: yes
-streisand_tor_enabled: no
-streisand_wireguard_enabled: yes
-
-# Definitions needed for Let's Encrypt HTTPS (or TLS) certificate setup.
-#
-# If these are both left as empty strings, Let's Encrypt will not be set up and
-# a self-signed certificate will be used instead.
-#
-# The domain to use for Let's Encrypt certificate.
-streisand_domain_var: ""
-# The admin email address for Let's Encrypt certificate registration.
-streisand_admin_email_var: ""
-
-
-EOF_VPNSERVER
-}
-
-installStreisand()
-{
-  ssh-keygen -f id_rsa -t rsa -N ''
-  deploy/streisand-local.sh --site-config global_vars/noninteractive/my-vpn-server.yml
-}
-
-
-updateOs()
-insallPythonChain()
-bootstrapStreisand()
-configureStreisand()
-installStreisand()
+ssh-keygen -f id_rsa -t rsa -N ''
+deploy/streisand-local.sh --site-config global_vars/noninteractive/my-vpn-server.yml
